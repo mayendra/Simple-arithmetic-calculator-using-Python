009c095b5736b655bc4eeed1b14da460737ca5c1
mayendra <70875140+mayendra@users.noreply.github.com>
2023-07-21 17:20:53 +0700

    Add files via upload

---
+++ Simple Calculator.py
@@ -0,0 +1,40 @@
+
+""" Simple calculator for add, subtract,multiply and divide with function"""
+
+# add function
+def add(x, y):
+   return x + y
+# subtract function
+def subtract(x, y):
+   return x - y
+# multiply function
+def multiply(x, y):
+   return x * y
+# divide function
+def divide(x, y):
+   return x / y
+
+# operation menu
+print("=====Select Operation=====")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+# collect input form user 
+choice = input("Select (1/2/3/4): ")
+num1 = int(input("Type Your First Number : "))
+num2 = int(input("Type Your Second Number : "))
+if choice == '1':
+   print(num1,"+",num2,"=", add(num1,num2))
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2))
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+elif choice == '4':
+   try:
+       print(num1,"/",num2,"=", divide(num1,num2))
+   except:
+       print("input salah")
+else:
+   print("Input salah")
